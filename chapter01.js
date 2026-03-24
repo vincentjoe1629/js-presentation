73f0ada4e4089048fe1ffc4d2ca77cb611aeb4ac
vincentjoe1629 <vincentjoe1629@gmail.com>
2026-03-24 00:36:30 -0400

    Add files via upload

---
+++ chapter01.js
@@ -0,0 +1,13 @@
+console.log("Edjahton"); 
+console.log(26);
+
+
+console.log("Addition:", 6 + 3);
+console.log("Subtraction:", 6 - 3);
+console.log("Multiplication:", 6 * 3);
+console.log("Division:", 6 / 3);
+
+
+console.log(4 + 5);    
+console.log("4 + 5");   
+console.log("4" + "5"); 
